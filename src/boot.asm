8e89f8d936e64fafcc1669dc195ee8458d81a1c5
Jose Manuel Barajas Ramirez <barajas.jose.3d@gmail.com>
2025-07-20 19:44:36 -0600

    Initial commit of Peach OS kernel project

---
+++ src/boot.asm
@@ -0,0 +1,49 @@
+ORG 0
+BITS 16
+
+_start:
+    jmp short start
+    nop
+
+times 33 db 0
+
+start:
+    jmp 0x7C0:step2
+
+step2:
+    cli     ; Clear interrupts
+    sti     ; Enable interrupts
+    mov ax, 0x7C0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7C00
+    sti     ; Enable interrupts
+    mov si, message
+
+    mov si, message
+    call print
+    jmp $
+
+print:
+    mov bx, 0
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message: db 'Hello World!', 0
+
+times 510-($-$$) db 0
+
+dw 0xAA55
