f29686eeab7248ff60b883d04abff68c2efdb2bd
Jose Manuel Barajas Ramirez <barajas.jose.3d@gmail.com>
2025-08-07 21:18:43 -0600

    Enhanced code for Protected Mode

--- src/boot.asm
+++ src/boot.asm
@@ -1,65 +1,69 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
     jmp short start
     nop
 
-times 33 db 0
-
+ times 33 db 0
+ 
 start:
-    jmp 0x7C0:step2
+    jmp 0:step2
 
 step2:
-    cli     ; Clear interrupts
-    sti     ; Enable interrupts
-    mov ax, 0x7C0
+    cli ; Clear Interrupts
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
-    mov sp, 0x7C00
-    sti     ; Enable interrupts
+    mov sp, 0x7c00
+    sti ; Enables Interrupts
 
-    mov ah, 2 ; Read sector command
-    mov al, 1 ; One sector to read
-    mov ch, 0 ; Cylindey low eigth bits
-    mov cl, 2 ; Read sector two
-    mov dh, 0 ; Head number
-    mov bx, buffer 
-    int 0x13
-    jc error
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    mov si, buffer
-    call print
-    
+; Global Descriptor Table (GDT)
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+; offset 0x8
+gdt_code:     ; CS should pOINT to this
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+; offset 0x10
+gdt_data:      ; DS, SS, ES, FS, GS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start
+ 
+[BITS 32]
+load32:
     jmp $
 
-error:
-    mov si, error_message
-    call print
-    jmp $
 
-print:
-    mov bx, 0
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-error_message: db 'Failed to load sector', 0
-
-times 510-($-$$) db 0
-
+times 510-($ - $$) db 0
 dw 0xAA55
-
-buffer: db 'Hello world!'
